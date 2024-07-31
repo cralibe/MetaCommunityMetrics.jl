408d602122e17389325f06b4d9d02d6b1e2b4dd7
cralibe <cralibe@users.noreply.github.com>
2024-07-31 18:23:42 +0000

    deploy: 2496d38454723a446bcedc57d73701d46a015292

--- test/runtests.jl
+++
@@ -1,7 +0,0 @@
-using MetaCommunityMetrics
-using Test
-
-@testset "MetaCommunityMetrics.jl" begin
-    @test MetaCommunityMetrics.greet_your_package_name() == "Hello MetaCommunityMetrics!"
-    @test MetaCommunityMetrics.greet_your_package_name() != "Hello world!"
-end
